d20bb6cb934a6073221f8bcd753fc6e34e48403d
joetsoi <joe.yeung.tsoi@gmail.com>
2014-09-04 17:37:41 +0100

    fix request status error handling

--- ckanext/glasgow/controllers/request_status.py
+++ ckanext/glasgow/controllers/request_status.py
@@ -9,20 +9,20 @@ class RequestStatusController(toolkit.BaseController):
             'model': model,
             'session': model.Session,
         }
+        extra_vars = {}
         try:
             request_status = toolkit.get_action('get_change_request')(context,
                 {'id': request_id})
+            if not request_status:
+                toolkit.abort(404, toolkit._(
+                    'Request {0} not found'.format(request_id)))
+            extra_vars['request_status'] = request_status,
+        except ECAPIError, e:
+            helpers.flash_error('{0}'.format(e.error_dict['message']))
+            toolkit.abort(
+                502,
+                'Error fetching request from CTPEC Plaftform {}'.format(str(e))
+            )
         except toolkit.ValidationError, e:
             helpers.flash_error('{0}'.format(e.error_dict['message']))
-        except ECAPIError:
-            helpers.flash_error('{0}'.format(e.error_dict['message']))
-
-        if not request_status:
-            toolkit.abort(404, toolkit._(
-                'Request {0} not found'.format(request_id)))
-
-        extra_vars = {
-            'request_status': request_status,
-        }
-
         return toolkit.render('request_status.html', extra_vars=extra_vars)
